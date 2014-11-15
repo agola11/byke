6f1ed80393babd25e94cd973d3fef9f51ec9ffff
Ankush Gola <ankush@nat-oitwireless-inside-vapornet100-c-25246.princeton.edu>
2014-11-15 01:28:46 -0500

    added byke.ino file

---
+++ byke/byke.ino
@@ -0,0 +1,18 @@
+/* Byke: an automatic bike transmission.
+   Designed and developed by Joseph Bolling, Ankush Gola, and Ted Brundage.
+*/
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+
+}
+
+void update_count() 
+{
+
+}
